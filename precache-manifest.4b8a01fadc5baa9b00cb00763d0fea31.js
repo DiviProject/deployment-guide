497dfad6dce8a8ce383aca9c5e6ce0a60b21bf1a
Chris Cates <hello@chriscates.ca>
2019-12-12 20:15:01 -0500

    Updates

---
+++ precache-manifest.4b8a01fadc5baa9b00cb00763d0fea31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a8a92ec7e1ddbeb2b71d5ef272218f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "181a779d1e765ff7857f",
+    "url": "/static/css/main.cce92e88.chunk.css"
+  },
+  {
+    "revision": "f94b04f4ea3cd4f91080",
+    "url": "/static/js/2.b2674153.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.b2674153.chunk.js.LICENSE"
+  },
+  {
+    "revision": "181a779d1e765ff7857f",
+    "url": "/static/js/main.ffa6cf63.chunk.js"
+  },
+  {
+    "revision": "b858bb1a7f4a47162361",
+    "url": "/static/js/runtime-main.485c14cf.js"
+  }
+]);
